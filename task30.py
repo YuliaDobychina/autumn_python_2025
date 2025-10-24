f259b9fcf4366e99189fd2dada328f949c9fcc3f
Dobychina_Juliya <y.folk@mail.ru>
2025-10-24 18:42:15 +0300

    Update task30.py

--- task30.py
+++ task30.py
@@ -2,14 +2,14 @@
 # Создайте список числовых значений цен,  игнорируя некорректные записи.
 # Все цены переведите в рубли. Задачу следует решить с использованием списковых включений.
 
+currency = {'USD':80.98 ,'$':80.98,'€':94.58, '¥':0.5396,'₽':1}
+
+# Исходный набор данных
 prices = ["₽1500", "20.50 USD", "invalid", "€25.00", "$15.99",  "₽18.99", "N/A", "¥5000"]
 
-currency = {'USD':80.98 ,'$':80.98,'€':94.58, '¥':0.5396,'₽':1}
+# Преобразование данных
+prices = [price.replace('USD','$') for price in prices]
 
 prices_ruble = {price : float(price.replace(item,'')) * currency[item] for price in prices for item in price if item in currency.keys()}
 
 print(prices_ruble)
-
-
-
-
